0bab69080c495f2c655951493dd9a74ecdfae71a
Dongni An <13816554795@163.com>
2025-05-04 13:02:46 +0000

    Isolate the routes from main() and make main() only used for parsing runtime config, dependencies injection and run app

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -12,32 +12,21 @@ type application struct {
 }
 
 func main() {
+	// Pasrse the runtime configuration settings for the application
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	// Establish the dependencies for the handlers
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 	}))
-
 	app := &application{
 		logger: logger,
 	}
-	// Create a new ServeMux
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	// Register the home handler function for the root URL path
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
-	mux.HandleFunc("GET /snippet/foo/{path}", app.snippetCreateTest)
 
 	// Start the HTTP server on port 4000 and use the mux as the handler
 	logger.Info("Starting server", "addr", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	if err != nil {
 		logger.Error(err.Error())
 		os.Exit(1)
---
+++ cmd/web/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) routes() *http.ServeMux {
+	// Create a new ServeMux
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	// Register the home handler function for the root URL path
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView)
+	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	mux.HandleFunc("GET /snippet/foo/{path}", app.snippetCreateTest)
+
+	return mux
+}
